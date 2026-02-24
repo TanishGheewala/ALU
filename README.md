de0750646bfb2fc71a6dd4f8f00952f43ee0312e
Tanish <tanishgheewala@gmail.com>
2026-02-24 13:45:46 -0800

    Testing IcarusVerilog & GTKWave

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ALU
 A Parametric Arithmetic Logic Unit (ALU) suitable for integration into a simple CPU datapath.
+
+Uses IcarusVerilog, GTKWave
+1. run code using verilog extension
+2. run gtkwave wave.vcd
--- tb_alu.v
+++ tb_alu.v
@@ -1,7 +1,37 @@
 `timescale 1ns/1ps
-module tb_alu;
 
+module tb;
+  reg clk;
+  reg rst;
+  reg toggle;
+  reg [3:0] count;
 
-end
+  initial begin
+    clk = 0;
+    forever #5 clk = ~clk;
+  end
+
+  initial begin
+    rst    = 1;
+    toggle = 0;
+    count  = 0;
+
+    $dumpfile("wave.vcd");
+    $dumpvars(0, tb);
+
+    #12 rst = 0;
+
+    #100 $finish;
+  end
+
+  always @(posedge clk) begin
+    if (rst) begin
+      count  <= 0;
+      toggle <= 0;
+    end else begin
+      count  <= count + 1;
+      toggle <= ~toggle;
+    end
+  end
 
 endmodule
