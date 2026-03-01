06f6610ba61572c4aea25742c9f00e4c2e6c3b33
Tanish <tanishgheewala@gmail.com>
2026-03-01 15:23:25 -0800

    Skeleton

--- tb_alu.sv
+++ tb_alu.sv
@@ -2,5 +2,19 @@
  // Tanish Gheewala - March 2026
  // Testbench for the Parametric Arithmetic Logic Unit (ALU) at the RTL Level
 
+`timescale 1ns/1ps
 
- 
+module tb_alu;
+
+// Parameters
+
+// DUT inputs
+
+// DUT outputs
+
+// Checks
+
+    end
+
+
+endmodule
